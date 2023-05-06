eb9fc56c2a294f85313513ec9cb1a71eaeefa2ca
Nathan Salapat <nathan@nathansalapat.com>
2023-05-05 22:48:08 -0500

    updated license information.

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
---Load File
+--Load Files
 dofile(minetest.get_modpath('survival')..'/crafting.lua')
 dofile(minetest.get_modpath('survival')..'/nodes.lua')
 dofile(minetest.get_modpath('survival')..'/tools.lua')
